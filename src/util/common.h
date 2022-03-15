8703ff8cb85bc18025d011806ef3b6780c87fa18
tungfaifong <iam@tungfaifong.com>
2022-03-15 11:24:34 +0800

    新增common

--- src/util/common.h
+++ src/util/common.h
@@ -25,14 +25,14 @@ static constexpr uint32_t USRV_VERSION = 0001000;
 #define Mi Ki Ki
 
 using intvl_t = uint64_t;
-
 using NETID = size_t;
 using IP = std::string;
 using PORT = uint16_t;
-
 using TIMERID = size_t;
 
 static constexpr NETID INVALID_NET_ID = -1;
+static const IP DEFAULT_IP {""};
+static constexpr PORT DEFAULT_PORT = 0;
 static constexpr TIMERID INVALID_TIMER_ID = -1;
 
 static constexpr uint16_t MESSAGE_HEAD_SIZE = sizeof(uint16_t);
